c4642d6deea6c198ebeff9d3285b0264cb8c6da9
Gilang Reza Pahllevi <88469413+GilangRezaPahlevi@users.noreply.github.com>
2021-12-27 22:49:21 +0700

    Add files via upload

--- js1.js
+++ js1.js
@@ -1,6 +1,10 @@
 function frist(){
-        document.getElementById("mati").style.display="block";
+    document.getElementById("mati").style.display="block";
+    document.getElementById("hn").style.display="none";
+    document.getElementById("2").style.display="block";
 }
 function f1(){
     document.getElementById("mati").style.display="none";
+    document.getElementById("hn").style.display="block";
+    document.getElementById("2").style.display="none";
 }
